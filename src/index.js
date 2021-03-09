739fc259349328a2edcbc1f94bc17b91e2564a4d
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-09 21:35:16 +0000

    cpf validation

--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@ app.use(express.json());
 
 const customers = [];
 
+
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
+
+    const customersAlreadyExist = customers.some((customer) => customer.cpf === cpf);
+
+    if(customersAlreadyExist){
+        return response.status(400).json({error: "Customer already exists"});
+    }
 
     customers.push({
         cpf,
