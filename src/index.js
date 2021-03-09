ef4f8c32f724199fa0b38d61520ff4f7b75f6831
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-09 21:59:39 +0000

    Creating Middlewares

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
 
+    if(!customer){
+        return response.status(400).json({error: "Customer not found"});
+    }
+    request.customer = customer;
+    return next();
+}
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
@@ -29,15 +38,8 @@ app.post("/account", (request, response) => {
 
 })
 
-app.get("/statement", (request, response) => {
-    const { cpf } = request.headers;
-
-    const customer = customers.find(customer => customer.cpf === cpf)
-
-    if(!customer){
-        return response.status(400).json({error: "Customer not found"});
-    }
-
+app.get("/statement", verifyIfExistsAccountCPF,  (request, response) => {
+    const { customer } = request;
     return response.json(customer.statement)
 })
 
