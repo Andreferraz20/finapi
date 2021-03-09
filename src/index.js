0819e2f74ed89018e6fb49f00f09d29d96dbab67
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-09 21:25:11 +0000

    creating account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+const { request } = require('express');
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+    return response.status(201).send()
+
+})
 
 app.listen(3333)
