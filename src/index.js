edcce541e9f6e90a2c1017673f82de4aaec68375
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-09 21:45:20 +0000

    statement

--- src/index.js
+++ src/index.js
@@ -29,4 +29,12 @@ app.post("/account", (request, response) => {
 
 })
 
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    return response.json(customer.statement)
+})
+
 app.listen(3333)
